54c7be23efb490ac243ce71e53cf0e52c1e371f4
shalithacode <shalithalakruwan1998@gmail.com>
2022-02-28 22:33:39 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import {Navbar, NavDropdown, Nav, Container,Form, FormControl,Button} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
+import logo from './asset/logo192.png'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='head'>
+    <Navbar bg="dark" variant="dark" expand="lg" fixed="left">
+     
+  <Container fluid>
+    <Navbar.Brand ><img className='logo' src={logo} alt=''/></Navbar.Brand>
+    <Navbar.Toggle aria-controls="navbarScroll" />
+    <Navbar.Collapse id="navbarScroll">
+      <Nav
+        className="me-auto my-2 my-lg-0"
+        style={{ maxHeight: '100px' }}
+        navbarScroll
+      >
+        <Nav.Link href="#action1">Home</Nav.Link>
+        <Nav.Link href="#action2">About</Nav.Link>
+        <Nav.Link href="#action22">Contact</Nav.Link>
+        <NavDropdown title="Services" id="navbarScrollingDropdown">
+          <NavDropdown.Item href="#action3">Web</NavDropdown.Item>
+          <NavDropdown.Item href="#action4">Mobile</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action5">
+            Cloud Services
+          </NavDropdown.Item>
+        </NavDropdown>
+        
+      </Nav>
+      <Form className="d-flex">
+        <FormControl
+          type="search"
+          placeholder="Search"
+          className="me-2"
+          aria-label="Search"
+        />
+        <Button variant="info">Search</Button>
+      </Form>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+</div>
   );
 }
 
